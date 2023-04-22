9e27d241d8d5af897b4da2b3ffa0b05ece8af4e0
Norbert Hańderek <morph.gamedev@gmail.com>
2023-04-22 17:17:41 +0200

    Randomize bird color

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random=UnityEngine.Random;
 
 public class PlayerController : MonoBehaviour
 {
     public float strength = 1;
 
     private Rigidbody2D rb;
+
+    private void ShowRandomBirdColor()
+    {
+        foreach(Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+        
+        int randomChild = Random.Range(0,transform.childCount);
+        transform.GetChild(randomChild).gameObject.SetActive(true);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        ShowRandomBirdColor();
         rb = GetComponent<Rigidbody2D>();
     }
 
